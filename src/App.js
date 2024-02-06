ff43023bc555386356b393a2d73273db0ab563cc
David Proto <davidproto@sbcglobal.net>
2024-02-06 10:37:16 -0500

    questions finished and styles updated

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Question from './Components/Question';
 import Header from './Components/Header';
 import './App.css';
 import './index.css';
+const initialQuestionIndex = 0;
 
 const questionsData = [
   {
-    question: 'What is the capital of France?',
-    options: ['Berlin', 'Madrid', 'Paris', 'Rome'],
-    correctAnswer: 'Paris',
+    question:
+      "77 percent of the world's maple syrup is made in this Canadian Province.",
+    options: ['Alberta', 'Ontario', 'Manitoba', 'Quebec'],
+    correctAnswer: 'Quebec',
   },
   {
     question: 'Which planet is known as the Red Planet?',
@@ -67,9 +69,9 @@ const questionsData = [
     correctAnswer: 'Nile River',
   },
   {
-    question: 'What is the coldest continent in the world?',
-    options: ['Africa', 'Antarctica', 'Australia', 'North America'],
-    correctAnswer: 'Antarctica',
+    question: 'What is the name of the large river that flows through London?',
+    options: ['The Danube', 'The Rhine', 'The Thames', 'The Elbe'],
+    correctAnswer: 'The Thames',
   },
   {
     question: 'What is the capital of Washington State?',
@@ -80,6 +82,51 @@ const questionsData = [
     question: 'What is the largest country in Asia?',
     options: ['China', 'India', 'Japan', 'Indonesia'],
     correctAnswer: 'China',
+  },
+  {
+    question: "Where are 75% of the world's pineapples grown?",
+    options: ['India', 'Hawaii', 'Guam', 'Indonesia'],
+    correctAnswer: 'Hawaii',
+  },
+  {
+    question: 'Which country has the most natural lakes?',
+    options: ['USA', 'Canada', 'China', 'Australia'],
+    correctAnswer: 'Canada',
+  },
+  {
+    question: 'This country has the largest number of pyramids.',
+    options: ['Nigeria', 'Ivory Coast', 'Sudan', 'Eygpt'],
+    correctAnswer: 'Sudan',
+  },
+  {
+    question: 'In what state in the Grand Canyon located?',
+    options: ['Arizona', 'California', 'Nevada', 'Utah'],
+    correctAnswer: 'Arizona',
+  },
+  {
+    question: 'What is the administrative capital of South Africa?',
+    options: ['Cape Town', 'Johannesburg', 'Durban', 'Pretoria'],
+    correctAnswer: 'Pretoria',
+  },
+  {
+    question: 'What is the capital of Canada?',
+    options: ['Ottawa', 'Toronto', 'Vancouver', 'Calgary'],
+    correctAnswer: 'Ottawa',
+  },
+  {
+    question: 'What is the capital of Australia?',
+    options: ['Sydney', 'Melbourne', 'Perth', 'Canberra'],
+    correctAnswer: 'Canberra',
+  },
+  {
+    question: 'What is the capital of India?',
+    options: ['Mumbai', 'New Delhi', 'Chennai', 'Kolkata'],
+    correctAnswer: 'New Delhi',
+  },
+  {
+    question: 'Where is Zion National Park located?',
+    options: ['Canada', 'Mexico', 'USA', 'Australia'],
+    correctAnswer: 'USA',
   },
 ];
 
@@ -123,6 +170,11 @@ const App = () => {
     setCurrentQuestionIndex(currentQuestionIndex - 1);
   };
 
+  const handleRestart = () => {
+    setCurrentQuestionIndex(initialQuestionIndex);
+    setScore(0);
+  };
+
   const percentage = calculatePercentage();
   const grade = calculateGrade(percentage);
 
@@ -130,7 +182,7 @@ const App = () => {
     <div>
       <Header />
       <div>
-        <p>Score: {score}</p>
+        <p id="score">Score: {score}</p>
       </div>
 
       {currentQuestionIndex < questionsData.length ? (
@@ -140,21 +192,22 @@ const App = () => {
         />
       ) : (
         <div>
-          <h2>Game Over!</h2>
-          <p>Your final score is: {percentage.toFixed(1)}%</p>
-          <p>Grade: {grade}</p>
+          <h1>Game Over!</h1>
+          <p id="f-score">Your final score: {percentage.toFixed(1)}%</p>
+          <p id="f-score">Your Grade: {grade}</p>
+          <button onClick={handleRestart}>Restart</button>
         </div>
       )}
 
       <div>
         {currentQuestionIndex > 0 && (
           <button id="previous-button" onClick={handlePrevious}>
-            Previous
+            ← Previous
           </button>
         )}
         {currentQuestionIndex < questionsData.length - 1 && (
           <button id="next-button" onClick={handleNext}>
-            Next
+            Next →
           </button>
         )}
       </div>
